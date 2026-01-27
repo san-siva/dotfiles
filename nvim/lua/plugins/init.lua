de0445350d45d0c9013df6efd70ff9df4cd99e20
Santhosh Siva <santhosh.siva.oa@gmail.com>
2026-01-27 14:33:20 +0530

    feat: updated markdown preview

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -87,10 +87,28 @@ require('lazy').setup({
   {
     'iamcco/markdown-preview.nvim',
     cmd = { 'MarkdownPreviewToggle', 'MarkdownPreview', 'MarkdownPreviewStop' },
-    build = 'cd app && npm install && git restore package.json', -- Key fix
-    init = function()
+    build = function()
+      vim.fn['mkdp#util#install']()
+    end,
+    config = function()
       vim.g.mkdp_filetypes = { 'markdown' }
       vim.g.mkdp_theme = 'light'
+      vim.g.mkdp_auto_close = 0
+      vim.g.mkdp_refresh_slow = 0
+      vim.g.mkdp_preview_options = {
+        mkit = {},
+        katex = {},
+        uml = {},
+        maid = {}, -- Enable mermaid diagrams
+        disable_sync_scroll = 0,
+        sync_scroll_type = 'middle',
+        hide_yaml_meta = 1,
+        sequence_diagrams = {},
+        flowchart_diagrams = {},
+        content_editable = 0,
+        disable_filename = 0,
+        toc = {}
+      }
     end,
     ft = { 'markdown' },
   },
